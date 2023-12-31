995255a20d2a7ab96c852c48b997a791e5a3d95b
Karoly Molnar <molnarkares@gmail.com>
2023-12-31 10:45:53 +0100

    Added copyright header

--- release.sh
+++ release.sh
@@ -5,10 +5,11 @@ rm -rf kicad-tinbox.zip
 mkdir kicad-tinbox
 mkdir kicad-tinbox/plugins
 cp *.py kicad-tinbox/plugins
-cp *.png kicad-tinbox/plugins
+cp images/tinbox_measure.png kicad-tinbox/plugins
+cp images/icon.png kicad-tinbox/plugins
+cp images/icon.png kicad-tinbox/resources
 cp mounting_pad.scad kicad-tinbox/plugins
 mkdir kicad-tinbox/resources
-cp icon.png kicad-tinbox/resources
 cp metadata.json kicad-tinbox
 cd kicad-tinbox || exit
 zip -r ../kicad-tinbox.zip .
