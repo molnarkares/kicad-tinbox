be24d57221d7e7dca6dec7bb2bfd48a913163880
Karoly Molnar <molnarkares@gmail.com>
2024-01-01 19:30:58 +0100

    fixed release script (misplaced icon.png)

--- release.sh
+++ release.sh
@@ -4,12 +4,12 @@ rm -rf kicad-tinbox
 rm -rf kicad-tinbox.zip
 mkdir kicad-tinbox
 mkdir kicad-tinbox/plugins
+mkdir kicad-tinbox/resources
 cp *.py kicad-tinbox/plugins
 cp images/tinbox_measure.png kicad-tinbox/plugins
 cp images/icon.png kicad-tinbox/plugins
 cp images/icon.png kicad-tinbox/resources
 cp mounting_pad.scad kicad-tinbox/plugins
-mkdir kicad-tinbox/resources
 cp metadata.json kicad-tinbox
 cd kicad-tinbox || exit
 zip -r ../kicad-tinbox.zip .
